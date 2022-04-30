2830b626d4bc79fa9873a08dfff903eb91224d8b
shroomsss <84424227+shroomsss@users.noreply.github.com>
2022-04-30 16:39:32 -0500

    Add files via upload

---
+++ loader.lua
@@ -0,0 +1,13 @@
+local gid = game.PlaceId
+local ids = {
+    cr = 7167319176,
+    eb = 1923555882,
+    crBackup = 9494337796,
+}
+
+if gid == ids.cr then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/shroomsss/main/main/chicago.lua"))()
+    
+elseif gid == ids.eb then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/shroomsss/main/main/eastbrickton.lua"))()
+end
